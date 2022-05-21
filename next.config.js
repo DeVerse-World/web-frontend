45df27b45b3b269b7e8096c3b62faca94dbe9288
lth08091998 <lth08091998@gmail.com>
2022-05-21 15:56:31 +0700

    Add prod config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
     reactStrictMode: true,
     distDir: 'build',
     env: {
-        REACT_APP_SERVER_HOST: "localhost:8080",
-        REACT_APP_SERVER_PROTOCOL: "http",
+        REACT_APP_SERVER_HOST: "api.deverse.world",
+        REACT_APP_SERVER_PROTOCOL: "https",
         // RPC_URL: "localhost:8545"
     },
    typescript: {ignoreBuildErrors: true,ignoreDuringBuilds: true,},
