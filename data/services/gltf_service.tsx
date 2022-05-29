4d25ee15bbc9f62cd819b0ac2f7a0f877ab8a4ad
lth08091998 <lth08091998@gmail.com>
2022-05-29 13:00:38 +0700

    Hotfix styling and mint NFT

---
+++ data/services/gltf_service.tsx
@@ -0,0 +1,10 @@
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+class GLTFService {
+    test() {
+        let loader: GLTFLoader = new GLTFLoader();
+        
+    }
+}
+
+export default new GLTFService();
