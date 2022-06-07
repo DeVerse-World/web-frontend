38951bcb26bcda8cdbecf4c5221ad11e142dee7e
Nam Nguyen <nam.nt1@teko.vn>
2022-06-08 00:23:50 +0700

    allow repeating animation

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, useEffect, useRef, useState } from "react";
-import { useGLTF, useAnimations, TransformControls, OrbitControls } from '@react-three/drei'
+import React, { Suspense, useEffect, useState } from "react";
+import { TransformControls, OrbitControls } from '@react-three/drei'
 import { Canvas, GroupProps, useFrame } from "@react-three/fiber";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 import ModelPreviewService from "../data/services/3d_model_service";
@@ -9,41 +9,46 @@ interface ModelViewerProps extends GroupProps {
   filePath: string;
 }
 
+interface ModelObjectProps extends GroupProps {
+  gltf: GLTF;
+}
+
 export default function ModelViewer(props: ModelViewerProps) {
-  return (
+  const [model, setModel] = useState<GLTF>(null);
+
+  useEffect(() => {
+    ModelPreviewService.load(props.filePath, (gltf) => {
+      setModel(gltf);
+    })
+  }, [])
+
+
+  return model && (
     <Canvas className='border-4 rounded-lg m-4' style={{ height: '50vh', width: '80vh' }}>
       <Suspense fallback={null}>
         <TransformControls showX={false} showY={false} showZ={false}>
-          <ModelObject filePath={props.filePath} {...props}/>
+          <ModelObject filePath={props.filePath} {...props} gltf={model} />
         </TransformControls>
-
       </Suspense>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <spotLight position={[-10, -10, 0]} angle={0.55} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <OrbitControls makeDefault />
+      <OrbitControls makeDefault zoomSpeed={2}/>
       {/* <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} /> */}
-      {/* <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} /> */}
+      {/* <PresentationControls speed={1.5} global zoom={0.7} polar={[-0.1, Math.PI / 4]}></PresentationControls> */}
     </Canvas>
   );
 }
 
 
-function ModelObject(props: ModelViewerProps) {
-  const [model, setModel] = useState<GLTF>(null);
+function ModelObject(props: ModelObjectProps) {
 
-  let animationMixer: AnimationMixer = null;
+  let animationMixer: AnimationMixer = new AnimationMixer(props.gltf.scene);
 
   useEffect(() => {
-    ModelPreviewService.load(props.filePath, (gltf) => {
-      setModel(gltf);
-      animationMixer = new AnimationMixer(gltf.scene);
-      console.log(gltf.animations)
-      gltf.animations.forEach((animation) => {
-        animationMixer.clipAction(animation).play();
-      })
+    props.gltf.animations.forEach((animation) => {
+      animationMixer.clipAction(animation).play();
     })
   }, [])
 
@@ -51,7 +56,7 @@ function ModelObject(props: ModelViewerProps) {
     animationMixer?.update(delta)
   })
 
-  return model && (
-    <primitive object={model.scene} position={[0, -1, 0]} {...props} />
+  return (
+    <primitive object={props.gltf.scene} position={[0, -1, 0]} {...props} />
   )
 }
