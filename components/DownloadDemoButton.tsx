6159ae06dc8cb2ac222d7590f4cac052da9def15
Nam Nguyen <nam.nt1@teko.vn>
2022-07-11 13:37:07 +0700

    improve UI on mobile

---
+++ components/DownloadDemoButton.tsx
@@ -0,0 +1,17 @@
+import { CSSProperties } from "react";
+import { AiFillWindows } from "react-icons/ai";
+
+function DownloadDemoButton(props: CSSProperties) {
+    return (
+        <button id="btn-download-app" {...props}
+            className="flex flex-row text-lg items-center cursor-pointer text-white rounded-md h-[50px] py-2 px-10 bg-deverse-gradient"
+            onClick={() => {
+                window.open("https://drive.google.com/file/d/1va5Nyvzbz0PfheMk2Ma10JVuN4rsGliH/view", "_blank")
+            }} >
+            <span className="me-2">Download</span>
+            <AiFillWindows fontSize="2rem" />
+        </button>
+    )
+}
+
+export default DownloadDemoButton;
--- pages/alpha/index.tsx
+++ pages/alpha/index.tsx
@@ -3,6 +3,7 @@ import { CSSProperties, useState } from "react";
 import { Nav, Tab } from "react-bootstrap";
 import { AiFillWindows } from "react-icons/ai";
 import Footer from "../../components/common/Footer";
+import DownloadDemoButton from "../../components/DownloadDemoButton";
 
 enum AlphaTab {
     INFO, VERSE
@@ -21,34 +22,24 @@ function Alpha() {
         <section className='flex flex-col justify-between min-h-[100vh]'>
             {/* <button onClick={() => router.push('/mint-nft')}>Test</button> */}
             <Tab.Container id="tabs-with-dropdown" defaultActiveKey={AlphaTab.INFO} >
-                <Nav className="flex flex-row justify-between  cursor-pointer text-xl bg-black align-middle py-2 px-8"
-                    activeKey={selectedTab}
-                    onSelect={setSelectedTab} >
-                    <span className="flex flex-row grow justify-evenly " >
-                        <Nav.Item className="grow text-center max-w-[150px] " style={selectedTab == AlphaTab.INFO ? selectedTabStyle : {}}>
-                            <Nav.Link className="text-white" eventKey={AlphaTab.INFO}>Info</Nav.Link>
-                        </Nav.Item>
+                <span className="flex flex-row justify-between bg-black">
+                    <Nav className="flex flex-row flex-grow justify-between  cursor-pointer text-xl  align-middle py-2 px-8"
+                        activeKey={selectedTab}
+                        onSelect={setSelectedTab} >
+                        <span className="flex flex-row grow justify-evenly " >
+                            <Nav.Item className="grow text-center max-w-[150px] " style={selectedTab == AlphaTab.INFO ? selectedTabStyle : {}}>
+                                <Nav.Link className="text-white" eventKey={AlphaTab.INFO}>Info</Nav.Link>
+                            </Nav.Item>
 
-                        {/* <NavDropdown id="nav-dropdown-within-tab" title="Listing" menuVariant="dark" color="black">
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.LISTING} eventKey={MarketplaceTab.LISTING}>All</Dropdown.Item>
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.TWO_D_IMAGE} eventKey={MarketplaceTab.TWO_D_IMAGE}>2D Image</Dropdown.Item>
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.SKIN} eventKey={MarketplaceTab.SKIN}>Character Skin</Dropdown.Item>
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.RACE} eventKey={MarketplaceTab.RACE}>Character Race</Dropdown.Item>
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.GAME_MODE} eventKey={MarketplaceTab.GAME_MODE}>Game Mode</Dropdown.Item>
-                        <Dropdown.Item active={visibleTab == MarketplaceTab.BOT_LOGIC} eventKey={MarketplaceTab.BOT_LOGIC}>Bot Logic</Dropdown.Item>
-                    </NavDropdown> */}
-                        <Nav.Item className="grow text-center max-w-[150px]" style={selectedTab == AlphaTab.VERSE ? selectedTabStyle : {}}>
-                            <Nav.Link className="text-white" eventKey={AlphaTab.VERSE}>Verses</Nav.Link>
-                        </Nav.Item>
-                    </span>
-                    {/* <Nav.Item className="flex flex-row items-center text-white rounded-3xl py-2 px-4 bg-deverse-gradient" onClick={() => {
-                        window.open("https://drive.google.com/file/d/1va5Nyvzbz0PfheMk2Ma10JVuN4rsGliH/view", "_blank")
-                    }} >
-                        <span className="me-2">Download</span>
-                        <AiFillWindows fontSize="1.5rem" />
-                    </Nav.Item> */}
+                            <Nav.Item className="grow text-center max-w-[150px]" style={selectedTab == AlphaTab.VERSE ? selectedTabStyle : {}}>
+                                <Nav.Link className="text-white" eventKey={AlphaTab.VERSE}>Verses</Nav.Link>
+                            </Nav.Item>
+                            
+                        </span>
+                    </Nav>
+                    <DownloadDemoButton/>
+                </span>
 
-                </Nav>
                 <Tab.Content className="grow flex">
                     <Tab.Pane eventKey={AlphaTab.INFO} className="grow">
                         <div className="flex justify-center items-center text-white p-4" >
@@ -60,28 +51,6 @@ function Alpha() {
                             <h1 >Coming soon</h1>
                         </div>
                     </Tab.Pane>
-                    {/* <Tab.Pane eventKey={MarketplaceTab.SKIN}>
-                        <NFTList data={nfts.filter(e => e.assetType == AssetType.SKIN)} onOpen={onOpenNFTDescription} />
-                    </Tab.Pane>
-                    <Tab.Pane eventKey={MarketplaceTab.RACE}>
-                        <NFTList data={nfts.filter(e => e.assetType == AssetType.RACE)} onOpen={onOpenNFTDescription} />
-                    </Tab.Pane>
-                    <Tab.Pane eventKey={MarketplaceTab.GAME_MODE}>
-                        <NFTList data={nfts.filter(e => e.assetType == AssetType.GAME_MODE)} onOpen={onOpenNFTDescription} />
-                    </Tab.Pane>
-                    <Tab.Pane eventKey={MarketplaceTab.BOT_LOGIC}>
-                        <NFTList data={nfts.filter(e => e.assetType == AssetType.BOT_LOGIC)} onOpen={onOpenNFTDescription} />
-                    </Tab.Pane>
-                    <Tab.Pane active={visibleTab == MarketplaceTab.MINT_NFT} eventKey={MarketplaceTab.MINT_NFT}>
-                        <CreateNftAssetSection
-                            fileUri={fileUri}
-                            onNftCreated={(assetType: AssetType, fileUri: string) => {
-                                //TODO: should we reload everything?
-                                loadNFTs(null);
-                                onSelectTab(MarketplaceTab.LISTING);
-                            }}
-                        />
-                    </Tab.Pane> */}
                 </Tab.Content>
             </Tab.Container>
             <Footer />
