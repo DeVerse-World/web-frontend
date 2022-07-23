6238624b785419b1a2a751bb1128bdfdfb1df2fd
lth08091998 <lth08091998@gmail.com>
2022-07-23 13:39:03 +0800

    Fix deploy script

--- deploy/build_staging.sh
+++ deploy/build_staging.sh
@@ -1,4 +1,5 @@
 cp ../conf/staging/fe/config.js ../config.js
 cp ../conf/staging/fe/next.config.js ../next.config.js
+cp ../conf/staging/fe/nginx_conf ../nginx_conf
 npm run build
 rsync -avz -e ssh ../* root@206.189.159.101:/root/repos/deploy/web-frontend-staging
