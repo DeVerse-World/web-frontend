362d71b1d574f7e68ee7a74d575618a3f73a4dc3
lth08091998 <lth08091998@gmail.com>
2022-07-30 19:00:53 +0800

    Change path wallet -> user

--- data/services/wallet_service.tsx
+++ data/services/wallet_service.tsx
@@ -7,8 +7,9 @@ import StorageService from "./storage_service";
 
 class WalletService {
     async getOrCreateWallet(session_key: string, wallet_address: string) {
-        return deverseClient.post('/wallet/getOrCreate', {
-            address: wallet_address,
+        return deverseClient.post('/user/getOrCreate', {
+            login_mode: "METAMASK",
+            wallet_address: wallet_address,
             session_key: session_key
         })
     }
@@ -34,10 +35,11 @@ class WalletService {
     async authMetamask(wallet_address: string, signature: string) {
         const response = await deverseClient({
             method: "post",
-            url: `/wallet/auth`,
+            url: `/user/auth`,
             data: {
-                address: wallet_address,
-                signature: signature,
+                login_mode: "METAMASK",
+                wallet_address: wallet_address,
+                wallet_signature: signature,
             },
             withCredentials: true
         });
@@ -47,11 +49,12 @@ class WalletService {
     async authLoginLink(session_key, wallet_address, signature) {
         const response = await deverseClient({
             method: "post",
-            url: `/wallet/authLoginLink`,
+            url: `/user/authLoginLink`,
             data: {
+                login_mode: "METAMASK",
                 session_key: session_key,
-                address: wallet_address,
-                signature: signature
+                wallet_address: wallet_address,
+                wallet_signature: signature
             },
             withCredentials: true
         });
