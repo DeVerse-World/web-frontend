0126ab3f035e2a489981afe61ee43728a8199616
lth08091998 <lth08091998@gmail.com>
2022-07-30 19:07:17 +0800

    Update deploy script

--- deploy/build_prod.sh
+++ deploy/build_prod.sh
@@ -1,5 +1,5 @@
-cp ../conf/prod/fe/config.js ../config.js
-cp ../conf/prod/fe/next.config.js ../next.config.js
-cp ../conf/prod/fe/nginx_conf ../nginx_conf
+cp ../conf/prod/fe/config.js ../config_temp.js
+cp ../conf/prod/fe/next.config.js ../next_temp.config.js
+cp ../conf/prod/fe/nginx_conf ../nginx_conf_temp
 npm run build
 rsync -avz -e ssh ../* root@206.189.159.101:/root/repos/deploy/web-frontend-prod
--- deploy/deploy_prod.sh
+++ deploy/deploy_prod.sh
@@ -1,2 +1,5 @@
+cd ..
+cp config_temp.js config.js
+cp next_temp.config.js next.config.js
 pm2 stop deverseFE
 pm2 start "npm start" --name deverseFE
