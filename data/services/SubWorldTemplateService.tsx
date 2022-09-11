433145c3127d0ab691ed633282a1a26a198387cb
Nam Nguyen <nam.nt1@teko.vn>
2022-09-11 12:54:28 +0700

    enable showing root templates

--- data/services/SubWorldTemplateService.tsx
+++ data/services/SubWorldTemplateService.tsx
@@ -1,7 +1,22 @@
 import deverseClient from "../api/deverse_client";
-import { Response } from "../model/response";
+import { Response, RootSubworldTemplateResponse } from "../model/response";
+import { BaseService } from "./BaseService";
 
-class SubWorldTemplateService {
+class SubWorldTemplateService extends BaseService {
+    async fetchTemplates() {
+
+    }
+
+    async fetchRootTemplates() {
+        let res = await deverseClient.get<Response<RootSubworldTemplateResponse>>('subworld/root_template');
+        return this.parseResponse(res);
+    }
+
+    async fetchDerivTemplates(rootId: string) {
+        let res = await deverseClient.get<Response<RootSubworldTemplateResponse>>(`subworld/root_template/${rootId}/deriv`);
+        return this.parseResponse(res);
+    }
+
     async deleteRootTemplate(rootTemplateId: string) {
         let res = await deverseClient.delete<Response<any>>(`subworld/root_template/${rootTemplateId}`);
         return res;
--- pages/subworlds/index.tsx
+++ pages/subworlds/index.tsx
@@ -1,34 +1,40 @@
 import { useEffect, useState } from "react";
 import NFTList from "../../components/asset/NFTList";
+import Footer from "../../components/common/Footer";
+import { AssetType } from "../../data/enum/asset_type";
 import { NFTAsset } from "../../data/model/nft_asset";
+import SubWorldTemplateService from "../../data/services/SubWorldTemplateService";
 
-function Subworlds() {
+function SubworldsPage() {
     const [nfts, setNfts] = useState<NFTAsset[]>([]);
 
     useEffect(() => {
-        // AccountService.getUserInfo().then(e => {
-        //     if (e.isSuccess && e.value) {
-        //         setData(e.value.avatars);
-        //         setNfts(e.value.avatars.map(item => {
-        //             let asset: NFTAsset = {
-        //                 id: item.id.toString(),
-        //                 file3dUri: item.preprocess_url,
-        //                 deletable: true,
-        //                 assetType: AssetType.AVATAR
-        //             }
-        //             return asset;
-        //         }))
-        //     }
-        // })
+        SubWorldTemplateService.fetchRootTemplates().then(res => {
+            if (res.isSuccess()) {
+                const data = res.value.subworld_templates.map<NFTAsset>(e => ({
+                    id: e.id.toString(),
+                    name: e.display_name,
+                    description: e.display_name,
+                    image: e.thumbnail_centralized_uri,
+                    fileAssetUriFromCentralized: e.thumbnail_centralized_uri,
+                    file2dUri: e.thumbnail_centralized_uri,
+                    fileAssetUri: e.level_ipfs_uri,
+                    file3dUri: e.level_ipfs_uri,
+                    assetType: AssetType.ROOT_SUBWORLD_TEMPLATE
+                }))
+                setNfts(data);
+            }
+        })
     }, [])
 
     return (
-        <div className="flex flex-col relativejustify-center items-center text-white p-4" >
+        <section id='section-content' className='flex flex-col justify-between '>
             <div className="flex flex-row gap-2">
                 <NFTList data={nfts} />
             </div>
-        </div>
+            <Footer />
+        </section>
     )
 }
 
-export default Subworlds;
+export default SubworldsPage;
