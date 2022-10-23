f900b12e777b5a9ab0323de9d68d637b10243968
Nam Nguyen <nam.nt1@teko.vn>
2022-10-23 11:16:41 +0700

    allow only download demo if this is a window

--- components/DownloadDemoButton.tsx
+++ components/DownloadDemoButton.tsx
@@ -1,4 +1,5 @@
 import { AiFillWindows } from "react-icons/ai";
+import { isDesktop } from 'react-device-detect';
 
 type Props = {
     className?: string
@@ -9,7 +10,12 @@ function DownloadDemoButton(props: Props) {
         <button id="btn-download-app"
             className={`flex flex-row text-lg items-center cursor-pointer text-white rounded-md px-8 m-1 bg-deverse-gradient ${props.className}`}
             onClick={() => {
-                window.open("https://drive.google.com/uc?export=download&id=1-uMt_0e8WHyMYDmd2lV3x6kxBD7DNVLU", "_blank")
+                if (isDesktop) {
+                    window.open("https://drive.google.com/uc?export=download&id=1-uMt_0e8WHyMYDmd2lV3x6kxBD7DNVLU", "_blank")
+                }
+                else {
+                    window.alert("Please open this in Window to download.")
+                }
             }} >
             <span id="btn-download-app" className="me-2">Download</span>
             <AiFillWindows id="btn-download-app" fontSize="2rem" />
