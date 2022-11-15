13e77be990c11c7d99ddde22bba7e537005c500b
Nam Nguyen <nam.nt1@teko.vn>
2022-11-15 20:58:56 +0700

    add pitch deck

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,15 @@ function Home(props) {
       <IntroSection />
       <HighlightFeatureSection />
       <ProductSection />
+      <div className='text-white p-4 flex flex-row justify-center'>
+        <a id='pitch-deck-btn' className="rounded-md bg-deverse-gradient w-[250px] 
+                uppercase cursor-pointer h-12 no-underline text-white
+                flex flex-row justify-center items-center"
+          href='https://www.canva.com/design/DAFJreBYRUI/rRSqsPiikMYP7wXohxTbWw/view?utm_content=DAFJreBYRUI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#5'
+          target="_blank">
+          <span className="text-lg font-black" >Pitch Deck</span>
+        </a>
+      </div>
       <BlogPostSection />
       <Footer />
     </div>
