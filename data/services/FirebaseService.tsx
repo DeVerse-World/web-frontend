8e42fb26c3579f16ec5e1b4efa65359fb74f81b1
Nam Nguyen <natenguyen9x@outlook.com>
2022-11-16 07:34:19 +0700

    Add blog post section (#57)
    * add line controller
    
    * add blotpost section to home
    
    * add pitch deck
    
    Co-authored-by: Nam Nguyen <nam.nt1@teko.vn>

--- data/services/FirebaseService.tsx
+++ data/services/FirebaseService.tsx
@@ -1,17 +1,44 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
+import { getRemoteConfig, getValue, RemoteConfig } from "firebase/remote-config";
+import { Firestore, getFirestore, collection, getDocs, Timestamp } from "firebase/firestore";
+import { BlogPost } from "../model/blog_post";
 
 class FirebaseService {
-    _app: FirebaseApp = initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID + ".firebaseapp.com",
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID + ".appspot.com",
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-    });
+    private _app: FirebaseApp;
+    private _config: RemoteConfig;
+    private _firestore: Firestore;
+    constructor() {
+        this._app = initializeApp({
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID + ".firebaseapp.com",
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID + ".appspot.com",
+            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+        });
+        this._firestore = getFirestore(this._app);
+        // this._config = getRemoteConfig(this._app);
+        // this._config.settings.minimumFetchIntervalMillis = 3600000;
+    }
 
-    dostuff() {
+    async getBlogPosts(): Promise<BlogPost[]> {
+        const blogPostDocs = await getDocs(collection(this._firestore, "blog_post"));
+        const blogPosts: BlogPost[] = [];
+        blogPostDocs.forEach((doc) => {
+            const data = doc.data();
+            const blogPost: BlogPost = {
+                title: data['title'],
+                uri: data['uri'],
+                thumbnail: data['thumbnail'],
+                id: doc.id,
+            }
+            if (data['created_at'] != null) {
+                blogPost.created_at = (data['created_at'] as Timestamp).toDate()
+            }
+            blogPosts.push(blogPost);
+        });
+        return blogPosts.sort((a, b) => a.created_at.getTime() > b.created_at.getTime() ? 1 : 0);
     }
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
 import IntroSection from '../components/home/IntroSection';
-import React, {  } from "react";
+import React, { useEffect } from "react";
 import ProductSection from '../components/home/ProductsSection';
 import WelcomeSection from '../components/home/WelcomeSection';
 import HighlightFeatureSection from '../components/home/HighlightFeaturesSection';
 import Footer from '../components/common/Footer';
+import BlogPostSection from '../components/home/BlogPostSection';
 
 function Home(props) {
+
   return (
     <div id='section-content'>
       <WelcomeSection />
       <IntroSection />
       <HighlightFeatureSection />
       <ProductSection />
+      <div className='text-white p-4 flex flex-row justify-center'>
+        <a id='pitch-deck-btn' className="rounded-md bg-deverse-gradient w-[250px] 
+                uppercase cursor-pointer h-12 no-underline text-white
+                flex flex-row justify-center items-center"
+          href='https://www.canva.com/design/DAFJreBYRUI/rRSqsPiikMYP7wXohxTbWw/view?utm_content=DAFJreBYRUI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#5'
+          target="_blank">
+          <span className="text-lg font-black" >Pitch Deck</span>
+        </a>
+      </div>
+      <BlogPostSection />
       <Footer />
     </div>
   );
