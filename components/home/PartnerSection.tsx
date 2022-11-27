c3a3776b98cf8ef28baaf80066e3f8ef2c5699a2
Nam Nguyen <nam.nt1@teko.vn>
2022-11-27 10:33:59 +0700

    unify styling for partner header

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -11,7 +11,7 @@ function PartnerSection(props) {
 
     return (
         <section id="section-partner" className="p-4 ">
-            <h3 className="text-center text-white text-6xl font-bold uppercase mb-12 mx-4 deverse-title">Partners</h3>
+            <h3 className="text-center text-6xl font-bold uppercase bg-deverse-gradient txt-deverse-gradient deverse-title">Partners</h3>
             <div className="flex flex-row flex-wrap gap-8 justify-center">
                 {partners.map(partner =>
                     <div className="w-[300px] mx-4 my-4 flex justify-center items-center">
