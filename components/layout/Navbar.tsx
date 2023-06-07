31e84dd7fa557936f644fc6bb97e36ae244632a3
Son Thanh Nguyen <linh.truongthuy0804@gmail.com>
2023-06-07 13:07:55 +0800

    Add AccountMenu

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,16 @@
-import { memo } from 'react'
+import { memo, useContext, useState } from 'react';
+import { AppContext } from '../contexts/app_context';
 import Image from 'next/image'
 import { Bars3Icon } from '@heroicons/react/20/solid'
+import AccountMenu from '../AccountMenu';
+import Button from '../Button';
 
-// TODO: Pass user as a prop to trigger re-rendering
 const Navbar = ({ setSidebarOpen }) => {
+    // This call is triggered twice on page load.
+    // The second time is in AccountMenu.tsx
+    const { user } = useContext(AppContext);
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center justify-between gap-x-6 border-b border-white/5 bg-gray-900 px-4 shadow-sm sm:px-6 lg:px-8">
             <button type="button" className="-m-2.5 p-2.5 text-white lg:hidden" onClick={() => setSidebarOpen(true)}>
@@ -22,12 +29,28 @@ const Navbar = ({ setSidebarOpen }) => {
 
             <div className="flex flex-1 items-center justify-end gap-x-8">
                 <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your profile</span>
-                    <img
-                        className="h-8 w-8 rounded-full bg-gray-800"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                    />
+                    {user
+                        ? (
+                            <>
+                                <span className="sr-only">Your profile</span>
+                                <Image
+                                    className="h-8 w-8 rounded-full bg-gray-800"
+                                    width={40}
+                                    height={40}
+                                    alt="avatar-img"
+                                    src={user.avatar || "/images/placeholder.webp"}
+                                    loader={() => user.avatar || "/images/placeholder.webp"}
+                                    onClick={() => setShowDropdown(true)}
+                                />
+                                {showDropdown && <AccountMenu onPointerLeave={() => setShowDropdown(false)} showDropdown={showDropdown} />}
+                            </>
+                        )
+                        : (
+                            <Button href="/login" primary>
+                                Login
+                            </Button>
+                        )
+                    }
                 </a>
             </div>
         </div>
