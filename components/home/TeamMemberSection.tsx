caaf797b7f6517fd73e87da1fb21f598edf291d6
blingblink <linh.truongthuy0804@gmail.com>
2023-06-15 11:35:12 +0700

    wip

--- components/home/TeamMemberSection.tsx
+++ components/home/TeamMemberSection.tsx
@@ -8,6 +8,14 @@ type CardProps = {
     data: TeamMember
 }
 
+// id: string;
+// name: string;
+// linkedin: string;
+// title: string;
+// education: string;
+// thumbnail: string;
+
+
 function TeamMemberCard(props: CardProps) {
     return (
         <div className={`w-[250px] text-white ${styles.nftCard}`}>
@@ -30,13 +38,46 @@ function TeamMemberCard(props: CardProps) {
     )
 }
 
-function TeamMemberSection(props) {
-    const [teamMember, setTeamMember] = useState<TeamMember[]>([])
-    const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
+function TeamMemberSection(props) {
+    const [team, setTeam] = useState<TeamMember[]>([])
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    
     useEffect(() => {
-        FirebaseService.getTeamMembers().then(setTeamMember)
+        FirebaseService.getTeamMembers().then(setTeam);
     }, [])
+
+    return (
+        <div>
+            <div className="mx-auto mt-32 max-w-xl lg:max-w-none px-6 sm:mt-40 lg:px-8">
+            <div className="px-4 max-w-2xl lg:mx-0">     
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl ">Founding team</h2>
+            <p className="mt-4 text-lg leading-8 text-gray-300">
+                Excepturi repudiandae alias ut. Totam aut facilis. Praesentium in neque vel omnis. Eos error odio. Qui
+                fugit voluptatibus eum culpa.
+            </p>
+            </div>
+            <ul
+            role="list"
+            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-9 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+            >
+            {team.map((person) => (
+                <li key={person.name}> 
+                <img className="aspect-[14/13] w-full rounded-2xl object-cover" src={person.thumbnail} alt="" />
+                <div className="mt-4 text-lg font-semibold leading-8 tracking-tight text-white">{person.name}</div>
+                <div  className="text-base leading-7 text-gray-300">{person.title}</div>
+                <div className="text-sm leading-6 text-gray-500">{person.education}</div>
+                <a href="https://www.linkedin.com/in/jaketran132/" className="text-sm font-semibold leading-6 text-brand">
+                                    Profile<span aria-hidden="true">&rarr;</span>
+                                </a>
+                </li>
+            ))}
+            </ul>
+        </div>
+        
+    </div>
+
+    );
 
     return (
         <section id="team-member" className="p-4 text-center flex flex-col items-center">
