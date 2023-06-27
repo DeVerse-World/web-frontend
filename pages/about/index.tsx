7790306e964ec3295f71e9d827d80540904ef346
blingblink <88964315+blingblink@users.noreply.github.com>
2023-06-28 01:27:13 +0800

    Fix display (#110)

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,9 +1,11 @@
+import ContentAbout from "../../components/about/ContentAbout";
 import AdvisorSection from "../../components/home/AdvisorSection";
 import TeamMemberSection from "../../components/home/TeamMemberSection";
 
 export default function About() {
     return (
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items'>
+            <ContentAbout/>
             <TeamMemberSection />
             <AdvisorSection />
         </div>
