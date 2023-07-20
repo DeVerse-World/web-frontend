e595fb380570b9aad48bf83006d2c510398e62fd
blingblink <linh.truongthuy0804@gmail.com>
2023-07-20 11:13:23 +0700

    wip

---
+++ components/BlogCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StarIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { getTimeString } from '../utils/time_util';
+
+const BlogCard = ({thumbnail, title, created_at, id  }) => {
+
+    return (
+        <div className="overflow-hidden rounded-lg bg-dark ring-1 ring-inset ring-medium text-left transition-all sm:my-4 sm:w-full sm:max-w-lg">
+            <div className="group text-sm">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-lg bg-gray-100">
+                    <img
+                        src={thumbnail}
+                        className="h-full w-full aspect-[12/10] object-cover object-center bg-darkest"
+                        loading="lazy"
+                    />
+                </div>
+               
+                <div className="max-w-xl">
+             
+                  <div className="mt-4 flex items-center gap-x-4 text-xs">
+                    <time dateTime={created_at} className="text-gray-500">
+                      {getTimeString(created_at)}
+                    </time>
+                  </div>
+           
+                 <div className="font-semibold">
+                      <Link href={`/blogs/${id}`} className="no-underline text-light">
+                        <span>
+                          <span className="absolute inset-0" />
+                          {title}
+                        </span>
+                      </Link>
+                    </div>
+               
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+export default BlogCard;
--- components/blog/BlogsPosts.tsx
+++ components/blog/BlogsPosts.tsx
@@ -1,6 +1,7 @@
 import FirebaseService from "../../data/services/FirebaseService";
 import { getTimeString } from "../../utils/time_util";
 import Link from "next/link";
+import BlogCard from "../BlogCard";
 
 export default function BlogsPosts({ blogs }) {
   return (
@@ -14,36 +15,13 @@ export default function BlogsPosts({ blogs }) {
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {blogs.map((blog) => (
-            <article key={blog.id} className="flex flex-col items-start">
-              <div className="relative w-full">
-                <img
-                  src={blog.thumbnail}
-                  alt={blog.title}
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-              </div>
-              <div className="max-w-xl">
-                {blog.created_at && (
-                  <div className="mt-4 flex items-center gap-x-4 text-xs">
-                    <time dateTime={blog.created_at} className="text-gray-500">
-                      {getTimeString(blog.created_at)}
-                    </time>
-                  </div>
-                )}
-                <div className="group relative">x
-                  <h3 className="mt-2 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <Link className="no-underline text-brand" href={`/blogs/${blog.id}`} >
-                      <div className="cursor-pointer text-brand hover:text-light">
-                        <span className="absolute inset-0 text-brand" />
-                        {blog.title}
-                      </div>
-                    </Link>
-                  </h3>
-                  {/* <p className="mt-4 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p> */}
-                </div>
-              </div>
-            </article>
+            <BlogCard 
+            thumbnail={blog.thumbnail}
+            title={blog.title}
+            created_at={blog.created_at}
+            id={blog.id}
+            />
+
           ))}
         </div>
       </div>
