f7cbc63620dfdf84d71cb7e360cc420ed971c0f4
blingblink <linh.truongthuy0804@gmail.com>
2023-08-07 10:40:41 +0700

    Change GA ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,13 +33,13 @@ function DeverseApp({ Component, pageProps }) {
               <meta name="keywords" content="metaverse, sandbox, unreal engine 5, deverse world, deverse, editor, building, simulation, gameplay, build game, develop game, creator, content, assets, nft, the sandbox, decentraland, roblox, multiverse, youtube, social, minecraft" />
               <meta property="og:site_name" content="Deverse World" />
               <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/deverse-357506.appspot.com/o/logo.webp?alt=media&token=67e233e2-fba3-4055-9050-41a2f36f759e" />
-              <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11255567082" />
+              <Script src="https://www.googletagmanager.com/gtag/js?id=G-XXF38ZMELH" />
                 <Script id="google-analytics">
                   {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', 'AW-11255567082');
+                    gtag('config', 'G-XXF38ZMELH');
                   `}
                 </Script>
             </Head>
