fe1efe44e7f581e5160406e3c351302c8d13c75f
blingblink <linh.truongthuy0804@gmail.com>
2023-08-11 14:33:49 +0700

    replace image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,11 +28,11 @@ function DeverseApp({ Component, pageProps }) {
           <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_LOGIN_CLIENT_ID || ''}>
             <Head>
               <title>Deverse World</title>
-              <meta name="title" content="Deverse World" />
+              <meta name="title" content="Deverse World Meow" />
               <meta name="description" content="Your portal to the endless virtual world experiences, owned by the users" />
               <meta name="keywords" content="metaverse, sandbox, unreal engine 5, deverse world, deverse, editor, building, simulation, gameplay, build game, develop game, creator, content, assets, nft, the sandbox, decentraland, roblox, multiverse, youtube, social, minecraft" />
               <meta property="og:site_name" content="Deverse World" />
-              <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/deverse-357506.appspot.com/o/logo.webp?alt=media&token=67e233e2-fba3-4055-9050-41a2f36f759e" />
+              <meta property="og:image" content="https://i.cbc.ca/1.5359228.1577206958!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/smudge-the-viral-cat.jpg" />
               <Script src="https://www.googletagmanager.com/gtag/js?id=G-XXF38ZMELH" />
                 <Script id="google-analytics">
                   {`
